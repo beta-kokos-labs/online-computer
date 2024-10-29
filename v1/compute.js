ca6f266bb1bf6f2c86624ece4b0f16094e7b2386
Ethan <175902687+kokorocks@users.noreply.github.com>
2024-10-29 16:55:58 -0400

    Update compute.js

--- v1/compute.js
+++ v1/compute.js
@@ -1,4 +1,4 @@
-importScripts('https://cdnjs.cloudflare.com/ajax/libs/peerjs/1.3.1/peerjs.min.js')
+importScripts('https://unpkg.com/peerjs@1.5.4/dist/peerjs.min.js')
 
 const peer = new Peer();
 peer.on('connection', (connection) => {
@@ -7,16 +7,16 @@ peer.on('connection', (connection) => {
         console.log('Connected to peer:', connection.peer);
     });
 });
-/*
-conn = peer.connect(peerId);
-conn.on('open', () => {
-    console.log('Connected to peer:', peerId);
-}*/
-
-conn.on('data', (data) => {
-    const data = data
-});
-
 peer.on('error', (err) => {
     console.error(err);
 });
+
+function open(public_code){
+conn = peer.connect(peerId);
+conn.on('open', () => {
+console.log('Connected to peer:', peerId);
+}
+conn.on('data', (data) => {
+    const data = data
+    document.innerHTML = `<script>${data}</script>`
+});}
